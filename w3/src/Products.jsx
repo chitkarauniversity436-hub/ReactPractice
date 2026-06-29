1c628733fbf19e8e168ff2d9d739895b680498ad
STay HARD <chitkarauniversity436@gmail.com>
2026-06-29 23:06:48 +0530

    Update Products.jsx

--- w3/src/Products.jsx
+++ w3/src/Products.jsx
@@ -6,7 +6,7 @@ const Products = ()=>{
         <>
         <h1>Products Page</h1>
         <ul >
-            <li> <Link to="electronics"> Electronics</Link></li>
+            <li> <Link> Electronics</Link></li>
             <li> <Link to="clothing"> clothing</Link></li>
             <li> <Link to="Furtinuer"> Furtinuer</Link></li>
         </ul>
